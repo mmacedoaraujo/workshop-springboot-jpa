a69d9123861cba2ec9aded271466b78d8ef3004d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-15 19:46:16 -0300

    initial commit

---
+++ src/main/java/com/mmacedoaraujo/workshopspringbootjpa/CourseSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mmacedoaraujo.workshopspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmacedoaraujo/workshopspringbootjpa/CourseSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmacedoaraujo.workshopspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
