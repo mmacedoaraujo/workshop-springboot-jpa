d003b52f50adbd629b7dbddfd00fce28e89ec4c9
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-20 15:37:38 -0300

    Added spring annotations

--- src/main/java/com/mmacedoaraujo/workshopspringbootjpa/entities/pk/OrderItemPK.java
+++ src/main/java/com/mmacedoaraujo/workshopspringbootjpa/entities/pk/OrderItemPK.java
@@ -3,13 +3,22 @@ package com.mmacedoaraujo.workshopspringbootjpa.entities.pk;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.mmacedoaraujo.workshopspringbootjpa.entities.Order;
 import com.mmacedoaraujo.workshopspringbootjpa.entities.Product;
 
+@Embeddable
 public class OrderItemPK implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@ManyToOne
+	@JoinColumn(name = "order_id")
 	private Order order;
+	@ManyToOne
+	@JoinColumn(name = "product_id")
 	private Product product;
 
 	public Order getOrder() {
