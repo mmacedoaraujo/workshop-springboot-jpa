5789d2ed8c2e1a728977257de6a6040e5ad1fd6b
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-22 14:12:21 -0300

    Corrected insert method in UserResource

--- src/main/java/com/mmacedoaraujo/workshopspringbootjpa/resources/UserResource.java
+++ src/main/java/com/mmacedoaraujo/workshopspringbootjpa/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.mmacedoaraujo.workshopspringbootjpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mmacedoaraujo.workshopspringbootjpa.entities.User;
 import com.mmacedoaraujo.workshopspringbootjpa.services.UserService;
@@ -37,8 +39,9 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = userService.insert(obj);
-
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getUserId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
