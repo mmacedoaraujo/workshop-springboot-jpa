36362db13308719ccdd0d2caed6853ad465d5ed8
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-23 20:08:27 -0300

    Treated EntityNotFouundException

--- src/main/java/com/mmacedoaraujo/workshopspringbootjpa/services/UserService.java
+++ src/main/java/com/mmacedoaraujo/workshopspringbootjpa/services/UserService.java
@@ -3,6 +3,8 @@ package com.mmacedoaraujo.workshopspringbootjpa.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +46,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = userRepo.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepo.save(entity);
+		try {
+			User entity = userRepo.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepo.save(entity);
+		} catch (EntityNotFoundException ex) {
+			throw new ResourceNotFoundException(id);
+		}
 
 	}
 
